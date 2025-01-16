7b22fb8b65801ab93172fc3bf1a36329ffe3ff50
aswad96 <110208996+aswad96@users.noreply.github.com>
2025-01-16 12:41:49 +0700

    Update backup.php

--- Bak/backup.php
+++ Bak/backup.php
@@ -1,13 +1,11 @@
 <?php
 function get_contents() {
-  $data file_get_contents("php://input");
+  $data = file_get_contents("php://input");
   // Tempat Simpennya
   $filename=date("d-m-Y");
   if (file_put_contents($filename, $data)) {
-    
   }
-  
-}
+  }
 get_contents();
 ?>
 
